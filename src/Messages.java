5db4684d78e393c822159376eb342f1d6bdcfcd7
iamchewinggum <santo2000x@gmail.com>
2025-08-24 01:40:49 -0700

    updated Message.java file :))

--- src/Messages.java
+++ src/Messages.java
@@ -1,10 +1,45 @@
 public class Messages {
 
 
+   final private String Dontworry = "dont worry your info is save with us" +
+            "one you are done with using our web application all information is deleted, nothing is saved";
 
+
+    //no constructor
+    /*
     Messages(){
 
     }
+     */
+
+   final private String thankyou = "Thank you for using us :)";
+
+
+   final private String hi = "hello :) welcome to InfoPack!, InfoPack was made so that you could have all of your family," +
+            " info on a single or couple of pages to allow anyone to fill out forms easier, by having all the information needed" +
+            "to fill our forms, helping out kids,teens,and adults fill out forms";
+
+
+
+
+    //getters
+
+    public String get_hiMessage(){
+        return hi;
+    }
+    public String get_Thankyou(){
+        return thankyou;
+    }
+
+
+
+
+    public String get_Dontworry(){
+        return Dontworry;
+    }
+
+
+
 
 
 
